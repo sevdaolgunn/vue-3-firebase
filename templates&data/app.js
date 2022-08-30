e7a1d71ea147658badb1a6c28150767bd63b3b8a
sevdaolgunn <sevdaolgun111@hotmail.com>
2022-08-30 23:42:29 +0300

    lesson0208

---
+++ templates&data/app.js
@@ -0,0 +1,12 @@
+const app= Vue.createApp({
+    data() {
+        return {
+            title: 'Fatmagülün Suçu Ne?',
+            director:'Hilal Saral ',
+            
+        }
+    }
+
+})
+
+app.mount('#app')
